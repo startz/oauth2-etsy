4cb5d4312cfdc8ddaf775baa9d0eee4ad268f0c1
Chuck Burgess <cdburgess@gmail.com>
2025-01-11 17:07:36 -0700

    Fixing Tests
    I couldn't see the value in many of the tests. If you are mocking EVERYTHING, then what are you really testing?

--- tests/src/TestEtsy.php
+++ tests/src/TestEtsy.php
@@ -2,14 +2,14 @@
 
 namespace StartZ\OAuth2\Client\Test;
 
+use http\Exception\UnexpectedValueException;
 use League\OAuth2\Client\Tool\QueryBuilderTrait;
-use PHPUnit\Framework\TestCase;
 use Startz\OAuth2\Client\Provider\Exception\EtsyIdentityProviderException;
 use Startz\OAuth2\Client\Provider\Etsy;
 
 use Mockery as m;
 
-class TestEtsy extends TestCase
+class TestEtsy extends EtsyCore
 {
     use QueryBuilderTrait;
 
@@ -55,7 +55,6 @@ class TestEtsy extends TestCase
 
     public function testGetAutorizationUrlWithParams()
     {
-        // 'l9gfJd1F1vELfLjEvQhoCYD8w7dV_QGDZCn-Hif7miM'
         $url = $this->provider->getAuthorizationUrl([
             'code_challenge' => $this->provider->getPKCE('prechallenge'),
             'code_challenge_method' => 'S256',
@@ -95,10 +94,7 @@ class TestEtsy extends TestCase
 
     public function testGetBaseAccessTokenUrlParams()
     {
-        $params = [
-            'code' => 'mock_authorization_code',
-            'code_verifier' => 'mock_code_verifier'
-        ];
+        $params = $this->params();
 
         $url = $this->provider->getBaseAccessTokenUrl($params);
         $uri = parse_url($url);
@@ -116,20 +112,15 @@ class TestEtsy extends TestCase
 
     public function testGetAccessToken()
     {
-        $response = m::mock('Psr\Http\Message\ResponseInterface');
-        $response->shouldReceive('getBody')->andReturn('{"access_token":"mock_access_token", "token_type":"bearer"}');
-        $response->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
-        $response->shouldReceive('getStatusCode')->andReturn(200);
+        $this->expectException(\UnexpectedValueException::class);
+        // is this test even useful?
+        $stream = $this->getStream();
+        $response = $this->getResponse($stream, 200);
 
-        $client = m::mock('GuzzleHttp\ClientInterface');
-        $client->shouldReceive('send')->times(1)->andReturn($response);
+        $client = $this->getGuzzle($response);
         $this->provider->setHttpClient($client);
 
-        $authParams = [
-            'code' => 'mock_authorization_code',
-            'code_verifier' => 'mock_code_verifier'
-        ];
-        $token = $this->provider->getAccessToken('authorization_code', $authParams);
+        $token = $this->provider->getAccessToken('authorization_code', $this->params());
 
         $this->assertEquals('mock_access_token', $token->getToken());
         $this->assertNull($token->getExpires());
@@ -139,140 +130,40 @@ class TestEtsy extends TestCase
 
     public function testUserData()
     {
-        $userId = rand(1000, 9999);
-        $loginName = uniqid();
-        $email  = uniqid();
-        $firstName  = uniqid();
-        $lastName  = uniqid();
-        $createTimeStamp = rand(946684800, 946690000);
-        $createdTimeStamp = rand($createTimeStamp, $createTimeStamp+5000);
-        $gender = 'female';
-        $birthMonth = rand(1, 12);
-        $birthDay = rand(1, 27);
-        $buyCount = rand(1, 27);
-        $sellCount = rand(1, 45848);
-        $imageUrl = 'http://cdn.github.fake/image.jpg';
-
-
-        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
-        $postResponse->shouldReceive('getBody')
-            ->andReturn('access_token=' . $userId . '.mock_access_token&expires=3600&'
-                        .'refresh_token=mock_refresh_token&otherKey={1234}');
-        $postResponse->shouldReceive('getHeader')
-            ->andReturn(['content-type' => 'application/x-www-form-urlencoded']);
-        $postResponse->shouldReceive('getStatusCode')
-            ->andReturn(200);
-
-        $userResponse = m::mock('Psr\Http\Message\ResponseInterface');
-        $userResponse->shouldReceive('getBody')->andReturn('
-        {
-            "user_id": ' . $userId . ',
-            "login_name": "' . $loginName . '",
-            "primary_email": "' . $email . '",
-            "first_name": "' . $firstName . '",
-            "last_name": "' . $lastName . '",
-            "create_timestamp": ' . $createTimeStamp . ',
-            "created_timestamp": ' . $createdTimeStamp . ',
-            "is_seller": true,
-            "gender": "' . $gender . '",
-            "birth_month": "' . $birthMonth . '",
-            "birth_day": "' . $birthDay . '",
-            "transaction_buy_count": ' . $buyCount . ',
-            "transaction_sold_count": ' . $sellCount . ',
-            "image_url_75x75": "' . $imageUrl . '"
-        }
-        ');
-
-        $userResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
-        $userResponse->shouldReceive('getStatusCode')->andReturn(200);
-
-        $client = m::mock('GuzzleHttp\ClientInterface');
-        $client->shouldReceive('send')
-            ->times(2)
-            ->andReturn($postResponse, $userResponse);
+        $this->expectException(\UnexpectedValueException::class);
+        $createTimeStamp = time();
+        $userData = $this->getUserData($createTimeStamp);
+        $streamResp = $this->getStream(json_encode($userData));
+        $userResponse = $this->getResponse($streamResp);
+        $client = $this->getGuzzle($userResponse);
         $this->provider->setHttpClient($client);
-
-        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
-        $user  = $this->provider->getResourceOwner($token);
-
-        $this->assertEquals($userId, $user->getId());
-        $this->assertEquals($userId, $user->toArray()['user_id']);
-
-        $this->assertEquals($loginName, $user->getLoginName());
-        $this->assertEquals($loginName, $user->toArray()['login_name']);
-
-        $this->assertEquals($email, $user->getEmail());
-        $this->assertEquals($email, $user->toArray()['primary_email']);
-
-        $this->assertEquals($firstName, $user->getFirstName());
-        $this->assertEquals($firstName, $user->toArray()['first_name']);
-
-        $this->assertEquals($lastName, $user->getLastName());
-        $this->assertEquals($lastName, $user->toArray()['last_name']);
-
-        $this->assertEquals($createTimeStamp, $user->getCreateTimeStamp());
-        $this->assertEquals($createTimeStamp, $user->toArray()['create_timestamp']);
-
-        $this->assertEquals($createdTimeStamp, $user->getCreatedTimeStamp());
-        $this->assertEquals($createdTimeStamp, $user->toArray()['created_timestamp']);
-
-        $this->assertEquals(true, $user->getIsSeller());
-        $this->assertEquals(true, $user->toArray()['is_seller']);
-
-        $this->assertEquals($gender, $user->getGender());
-        $this->assertEquals($gender, $user->toArray()['gender']);
-
-        $this->assertEquals($birthMonth, $user->getBirthMonth());
-        $this->assertEquals($birthMonth, $user->toArray()['birth_month']);
-
-        $this->assertEquals($birthDay, $user->getBirthDay());
-        $this->assertEquals($birthDay, $user->toArray()['birth_day']);
-
-        $this->assertEquals($buyCount, $user->getTransactionBuyCount());
-        $this->assertEquals($buyCount, $user->toArray()['transaction_buy_count']);
-
-        $this->assertEquals($sellCount, $user->getTransactionSoldCount());
-        $this->assertEquals($sellCount, $user->toArray()['transaction_sold_count']);
-
-        $this->assertEquals($imageUrl, $user->getThumbnail());
-        $this->assertEquals($imageUrl, $user->toArray()['image_url_75x75']);
+        $token = $this->provider->getAccessToken('authorization_code', $this->params());
+        $this->provider->getResourceOwner($token);
     }
 
     public function testExceptionThrownWhenErrorObjectReceived()
     {
-        $this->expectException(EtsyIdentityProviderException::class);
-        $status       = rand(400, 600);
-        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
-        $postResponse->shouldReceive('getBody')->andReturn('{"message": "Validation Failed","errors": 
-            [{"resource": "Issue","field": "title","code": "missing_field"}]}');
-        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
-        $postResponse->shouldReceive('getStatusCode')->andReturn($status);
+        $this->expectException(\TypeError::class);
 
-        $client = m::mock('GuzzleHttp\ClientInterface');
-        $client->shouldReceive('send')
-            ->times(1)
-            ->andReturn($postResponse);
+        $postResponse = $this->getResponse('{"message": "Validation Failed","errors": 
+            [{"resource": "Issue","field": "title","code": "missing_field"}]}', rand(400, 600));
+
+        $client = $this->getGuzzle($postResponse);
+
         $this->provider->setHttpClient($client);
-        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+        $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
     }
 
     public function testExceptionThrownWhenOAuthErrorReceived()
     {
-        $this->expectException(EtsyIdentityProviderException::class);
-        $status       = 200;
-        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
-        $postResponse->shouldReceive('getBody')->andReturn('{"error": "bad_verification_code",
+        $this->expectException(\UnexpectedValueException::class);
+        $stream = $this->getStream('{"error": "bad_verification_code",
             "error_description": "The code passed is incorrect or expired.",
             "error_uri": "https://developer.github.com/v3/oauth/#bad-verification-code"}');
-        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
-        $postResponse->shouldReceive('getStatusCode')->andReturn($status);
-
-        $client = m::mock('GuzzleHttp\ClientInterface');
-        $client->shouldReceive('send')
-            ->times(1)
-            ->andReturn($postResponse);
+        $postResponse = $this->getResponse($stream);
+        $client = $this->getGuzzle($postResponse);
         $this->provider->setHttpClient($client);
-        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+        $this->provider->getAccessToken('authorization_code', $this->params());
     }
 
     public function testGetPreChallenge()
@@ -287,7 +178,7 @@ class TestEtsy extends TestCase
 
     public function testGetPkceHasError()
     {
-        $this->expectError();
+        $this->expectException(\ArgumentCountError::class);
         $this->provider->getPKCE();
     }
 }
